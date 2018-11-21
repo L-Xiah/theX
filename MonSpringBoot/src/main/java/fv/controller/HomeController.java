c661fa5fb59ade225e45bef14b7bc02a36fd0dc8
XiaLiang <xialiang@shanghai-electric.com>
2018-11-21 14:56:59 +0800

    log4j2

--- MonSpringBoot/src/main/java/fv/controller/HomeController.java
+++ MonSpringBoot/src/main/java/fv/controller/HomeController.java
@@ -1,13 +1,20 @@
 package fv.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/index")
     public String index() {
+        System.out.println("--" + System.getProperty("catalina.home"));
+        System.out.println("--" + System.getProperty("user.home"));
+        logger.info("index---+++++");
         return "index";//通过mvc访问src/main/webapp路径下jsp文件（前面不加"/"）
     }
 
