687b3cdbc9926197211a339ecdd4b2a771da9d5a
XiaLiang <xialiang@shanghai-electric.com>
2018-11-27 22:25:26 +0800

    idea

--- SecSpringBoot/src/main/java/fv/Application.java
+++ SecSpringBoot/src/main/java/fv/Application.java
@@ -2,9 +2,18 @@ package fv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);//部署到tomcat上需要
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
